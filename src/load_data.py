7a699c0b244d37d61cf630f995e468190272da9d
Raza Lamb <rhlamb95@gmail.com>
2025-11-09 12:07:34 -0500

    adding kofi tag

--- src/load_data.py
+++ src/load_data.py
@@ -5,9 +5,3 @@ from src.lines import define_all_lines
 egresses = get_egresses()
 stations = load_all_stations(egresses)
 lines = define_all_lines(stations)
-
-
-for name, station in stations.items():
-    if name == "Pentagon":
-        for egress in station.egresses:
-            print(egress.__dict__)
